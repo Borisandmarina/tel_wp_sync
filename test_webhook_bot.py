24e1f5b12086773ea7903ba1391a3a22574957e4
Borisandmarina <104684620+Borisandmarina@users.noreply.github.com>
2025-05-01 21:54:35 +0200

    Update test_webhook_bot.py

--- test_webhook_bot.py
+++ test_webhook_bot.py
@@ -5,19 +5,22 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Webhook бот работает!")
+    await update.message.reply_text("Webhook бот работает!")
 
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-WEBHOOK_URL = os.environ.get("WEBHOOK_BASE_URL")  # https://your-bot.onrender.com
+WEBHOOK_BASE_URL = os.environ.get("WEBHOOK_BASE_URL")
+
+WEBHOOK_PATH = f"/webhook/{TELEGRAM_BOT_TOKEN}"
+WEBHOOK_URL = f"{WEBHOOK_BASE_URL}{WEBHOOK_PATH}"
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
-    print("🚀 Запуск вебхук-бота...")
+    print("Запуск тестового вебхук-бота...")
     app.run_webhook(
         listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),  # Render задаёт PORT
-        url_path=TELEGRAM_BOT_TOKEN,
-        webhook_url=f"{WEBHOOK_URL}/{TELEGRAM_BOT_TOKEN}"
+        port=10000,
+        webhook_path=WEBHOOK_PATH,
+        webhook_url=WEBHOOK_URL
     )
